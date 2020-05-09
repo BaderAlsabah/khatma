a8e1ab877ba879b06e3df78c7bf62e5f94e6762b
Bader Alsabah <Alsabah.dev@gmail.com>
2020-05-09 17:55:39 +0300

    Initial Commit

---
+++ khatma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  khatma
+//
+//  Created by Bader Alsabah on 5/9/20.
+//  Copyright © 2020 Bader Alsabah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
